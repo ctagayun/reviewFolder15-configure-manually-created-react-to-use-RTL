d30a044c771ed755d0404bc3e05273bbb3776337
Domingo <30532920+ctagayun@users.noreply.github.com>
2024-09-26 10:18:58 -0500

    Completed Setup Webpack 5 with Babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,44 @@
+
 const path = require('path');
 
 module.exports = {
-  //1
   entry: path.resolve(__dirname, './src/index.js'),
-  //2
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js']
+  },
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'bundle.js',
   },
-  //3
   devServer: {
     static: path.resolve(__dirname, './dist'),
   },
 };
+
+//const path = require('path');
+
+// module.exports = {
+//   //1
+//   entry: path.resolve(__dirname, './src/index.js'),
+//   //2
+//   output: {
+//     path: path.resolve(__dirname, './dist'),
+//     filename: 'bundle.js',
+//   },
+//   //3
+//   devServer: {
+//     static: path.resolve(__dirname, './dist'),
+//   },
+// };
 
 
 /* 
