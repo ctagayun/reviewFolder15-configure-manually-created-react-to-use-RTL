0da41fa79d3cf20258ccb34bf2f0e7dd49055f2e
Domingo <30532920+ctagayun@users.noreply.github.com>
2024-09-27 14:44:24 -0500

    Done with JEST Testing In React-Setup

--- src/App.spec.js
+++ src/App.spec.js
@@ -11,37 +11,37 @@ describe('My Test Suite', () => {
     });
   });
 
-describe('App', () => {
-    describe('Reducer', () => {
-        it('should set a list', () => {
-          const state = { list: [], error: null };
-          const newState = dataReducer(state, {
-            type: 'SET_LIST',
-            list,
-        });
-        expect(newState).toEqual({ list, error: null });
-      });
-      it('should reset the error if list is set', () => {
-        const state = { list: [], error: true };
-        const newState = dataReducer(state, {
-          type: 'SET_LIST',
-          list,
-        });
+// describe('App', () => {
+//     describe('Reducer', () => {
+//         it('should set a list', () => {
+//           const state = { list: [], error: null };
+//           const newState = dataReducer(state, {
+//             type: 'SET_LIST',
+//             list,
+//         });
+//         expect(newState).toEqual({ list, error: null });
+//       });
+//       it('should reset the error if list is set', () => {
+//         const state = { list: [], error: true };
+//         const newState = dataReducer(state, {
+//           type: 'SET_LIST',
+//           list,
+//         });
   
-        expect(newState).toEqual({ list, error: null });
-      });
+//         expect(newState).toEqual({ list, error: null });
+//       });
 
-      it('should set the error', () => {
-        const state = { list: [], error: null };
-        const newState = dataReducer(state, {
-          type: 'SET_ERROR',
-        });
+//       it('should set the error', () => {
+//         const state = { list: [], error: null };
+//         const newState = dataReducer(state, {
+//           type: 'SET_ERROR',
+//         });
   
-        expect(newState.error).toBeTruthy();
-      });
-    }); //eof describe reducer
+//         expect(newState.error).toBeTruthy();
+//       });
+//     }); //eof describe reducer
 
-  }); //eof describe app
+//   }); //eof describe app
 
 
 // describe('Counter', () => {
--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import App from './App';
 
 const title = 'React with Webpack and Babel. This is the output of React implementation src/index.js';
 
-ReactDOM.render(
-  <div>
-      <h1>
-      {title}
-      </h1>
-  </div>,
-  document.getElementById('app')
-);
+// ReactDOM.render(
+//   <div>
+//       <h1>
+//       {title}
+//       </h1>
+//   </div>,
+//   document.getElementById('app')
+// );
+
 module.hot.accept();
